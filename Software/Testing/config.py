084059410f3e7e9ac5df2d40cfa663caf57e7952
Bzzman <eyupbarlas2134@gmail.com>
2022-01-12 18:47:45 +0100

    Minor Update

--- Software/Testing/config.py
+++ Software/Testing/config.py
@@ -1,4 +1,4 @@
 
 #! Telegram Bot Tokens
 BOT_TOKEN = "5039844581:AAEJ3ZgnE3bFcljj_qduAGCCVhRC27I4k3A" # we can create a new bot and change this later
-BOT_CHATID = "-637535626"
+BOT_CHATID = "-1001793102646"
