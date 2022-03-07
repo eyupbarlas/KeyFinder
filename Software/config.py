95391ab41e4b43aaeed8302ab4ed6f380aa1269e
Eyüp Barlas <eyupbarlas2134@gmail.com>
2022-03-07 22:30:02 +0100

    Update config.py

--- Software/config.py
+++ Software/config.py
@@ -1,7 +1,7 @@
 # Configs
 #! Database Connection
-DB_TOKEN = "mongodb+srv://Bzzman:Eb914649.@cluster0.c39jc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DB_TOKEN = "your db token"
 
 #! Telegram Bot Tokens
-BOT_TOKEN = "5039844581:AAEJ3ZgnE3bFcljj_qduAGCCVhRC27I4k3A" # we can create a new bot and change this later
-BOT_CHATID = "-1001793102646"
+BOT_TOKEN = "your bot token" # we can create a new bot and change this later
+BOT_CHATID = "your chatid"
